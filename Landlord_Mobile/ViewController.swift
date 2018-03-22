4ac4484148707377042dd5c039178840f446e85d
5911 <jin.586@osu.edu>
2018-03-22 18:42:23 -0400

    Start up new branch

--- Landlord_Mobile/ViewController.swift
+++ Landlord_Mobile/ViewController.swift
@@ -10,18 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    // MARK: Login
+    @IBAction func userName(_ sender: UITextField) {
+    }
+    @IBAction func signIn(_ sender: UIButton) {
+    }
+
+    
+    
+    // MARK: Properties
+    @IBOutlet weak var addPropStackView: UIStackView!
+    @IBOutlet weak var listPropBtn: UIButton!
+    @IBOutlet weak var listPropView: UITextView!
+    
+    
+    // MARK: Owner Details
+
+    
+    
+    
+    
 //    @IBAction func listProp(_ sender: UIButton) {
 //        client.propertiesGet()
 //    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Set up the AWS client
         let credentialsProvider = AWSCognitoCredentialsProvider(regionType: AWSRegionType.USEast2, identityPoolId: "my_pool_id")
         let configuration = AWSServiceConfiguration(region: AWSRegionType.USEast2, credentialsProvider: credentialsProvider)
         AWSServiceManager.default().defaultServiceConfiguration = configuration
         let client = LANDLORDMOBILEAPIForPropertyMgmtClient.default()
+        
+        print(client.propertiesGet())
 
-        client.propertiesGet()
-        // Do any additional setup after loading the view, typically from a nib.
     }
     
     
@@ -30,6 +53,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
+    // MARK: Actions
+    @IBAction func addBtn(_ sender: UIButton) {
+    }
+    @IBAction func cancelBtn(_ sender: UIButton) {
+    }
+    @IBAction func listPropBtnPressed(_ sender: UIButton) {
+        listPropView.isHidden = true
+    }
+    
+    
+    
+    
 }
 
